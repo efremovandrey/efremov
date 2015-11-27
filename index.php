4faf4f9e42c672a746e64eb080d1461c93a6b767
efremovandrey <ef007@yandex.ru>
2015-11-27 10:58:53 +0300

    Update index.php

--- index.php
+++ index.php
@@ -56,21 +56,21 @@ if(!empty($_POST)) {
 try {
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $date = date("Y-m-d");
+    $data = date("Y-m-d");
     // Insert data
     $sql_insert = 
-"INSERT INTO registration_tbl (name, email, date) 
+"INSERT INTO registration_tbl (name, email, data) 
                    VALUES (?,?,?)";
     $stmt = $conn->prepare($sql_insert);
     $stmt->bindValue(1, $name);
     $stmt->bindValue(2, $email);
-    $stmt->bindValue(3, $date);
+    $stmt->bindValue(3, $data);
     $stmt->execute();
 }
 catch(Exception $e) {
     die(var_dump($e));
 }
-echo "<h3>Your're registered!</h3>";
+echo "<h3>Зарегестрированы</h3>";
 }
 ?>
 </body>
