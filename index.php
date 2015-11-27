05d7bd4567bb5cda9722fd9f14d0b454dbe0bc87
efremovandrey <ef007@yandex.ru>
2015-11-27 16:43:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,9 +11,9 @@
  Verdana, Helvetica, Sans-Serif;}
     h1, h2, h3,{ color: #000; 
 margin-bottom: 0; padding-bottom: 0; }
-    h1 { font-size: 1.75em; }
+    h1 { font-size: 2em; }
     h2 { font-size: 1.75em; }
-    h3 { font-size: 1.75em; }
+    h3 { font-size: 1.2em; }
     table { margin-top: 0.75em; }
     th { font-size: 1.2em;
  text-align: left; border: none; padding-left: 0; }
@@ -26,7 +26,7 @@ border: 0 none; }
 <p>Заполните ваше имя и адрес электронной почты, затем нажмите кнопку Отправить, чтобы зарегистрироваться.</p>
 <form method="post" action="index.php" 
 enctype="multipart/form-data" >
-      Имя  <input type="text" 
+      Имя    <input type="text" 
 name="name" id="name"/></br>
       Email <input type="text" 
 name="email" id="email"/></br>
