8d411b839fd912311cd5ae1f8a46aba3a5dffcab
efremovandrey <ef007@yandex.ru>
2015-11-27 16:42:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,13 +11,13 @@
  Verdana, Helvetica, Sans-Serif;}
     h1, h2, h3,{ color: #000; 
 margin-bottom: 0; padding-bottom: 0; }
-    h1 { font-size: 2em; }
+    h1 { font-size: 1.75em; }
     h2 { font-size: 1.75em; }
-    h3 { font-size: 1.2em; }
+    h3 { font-size: 1.75em; }
     table { margin-top: 0.75em; }
     th { font-size: 1.2em;
  text-align: left; border: none; padding-left: 0; }
-    td { padding: 0.25em 2em 0.25em 2em; 
+    td { padding: 0.25em 2em 0.25em 0em; 
 border: 0 none; }
 </style>
 </head>
